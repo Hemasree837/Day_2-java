24ec3a267c37fbbcccb9b6a3c1e0c664cad80a59
Hemasree837 <hemasree837@gmail.com>
2024-12-03 14:13:23 +0530

    Add files via upload

---
+++ Swap4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Swap4 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int b=sc.nextInt();
+        int g=sc.nextInt();
+        b=b^g;
+        g=b^g;
+        b=b^g;
+        String res=String.format("After Swapping\nboy=%d\ngirl=%d\n",b,g);
+        System.out.println(res);
+
+    }
+}
+    
+
