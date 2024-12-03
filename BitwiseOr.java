6b79f0950944a1f77f529dd2d667f6288eeae101
Hemasree837 <hemasree837@gmail.com>
2024-12-03 12:38:43 +0530

    Add files via upload

---
+++ BitwiseOr.java
@@ -0,0 +1,10 @@
+public class BitwiseOr {
+    public static void main(String args[])
+    {
+        System.out.println(11|18);
+        System.out.println(22|144) ;
+        System.out.println(88|122);
+        System.out.println(66|164);
+    }
+}
+
