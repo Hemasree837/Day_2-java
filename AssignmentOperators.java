906d4251f47086c65046af83cd77032fa67bf950
Hemasree837 <hemasree837@gmail.com>
2024-12-03 10:42:12 +0530

    Add files via upload

---
+++ AssignmentOperators.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class AssignmentOperators {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a+=b);
+        System.out.println(a-=b);
+        System.out.println(a*=b);
+        System.out.println(a/=b);
+        System.out.println(a%=b);
+        System.out.println(a+=6);
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(b-=3);
+        System.out.println(b++);
+        System.out.println(--b);
+
+
+
+    }
+}
