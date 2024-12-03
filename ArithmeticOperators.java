ca140fabcdda4bad95cab382edff8cd7fbee2e22
Hemasree837 <hemasree837@gmail.com>
2024-12-03 10:11:25 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,14 @@
+class ArithmeticOperators
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=15;
+        System.out.println("The addition of a and b is:" + (a+b));
+        System.out.println("The Subtraction of a and b is:" + (a-b));
+        System.out.println("The Multiplication of a and b is:" + (a*b));
+        System.out.println("The Division of a and b is:" + (a/b));
+        System.out.println("The Modulo Division of a and b is:" + (a%b));
+
+    }
+}
