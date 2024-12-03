3b96aad0b7826c5dbe37e82ccd0a254628c2a41a
Hemasree837 <hemasree837@gmail.com>
2024-12-03 12:02:09 +0530

    Add files via upload

---
+++ LogicalOperators.java
@@ -0,0 +1,11 @@
+public class LogicalOperators {
+    public static void main(String args[])
+    {
+        System.out.println(true&&true);
+        System.out.println(false||false);
+        System.out.println(!(true));
+        System.out.println(10>5 && 5<10);
+        System.out.println(10>5 || 5>10);
+        System.out.println(!(false));   
+    }
+}
